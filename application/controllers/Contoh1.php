dde04a9e942dd1fbc5b7abf165ec4c5f46b8dab0
RiskaHerdiantiWahida <riskaherdiantiw0511@gmail.com>
2021-09-27 10:17:38 +0700

    menambahkan biodata_tugas2

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,9 +4,8 @@ class Contoh1 extends CI_Controller
 	public function index()
 	{
 	    echo "<h1>Perkenalkan</h1>";
-		echo"Nama saya Imam Nawawi
-		     Saya tinggal di daerah Ciputat
-			 Olahraga yang saya sukai adalah
-			 Bulutangkis";
+		echo"Nama saya Riska Herdianti Wahida
+		     Saya tinggal di Ciamis
+			 No telpon 08748672466";
 	}
 }
